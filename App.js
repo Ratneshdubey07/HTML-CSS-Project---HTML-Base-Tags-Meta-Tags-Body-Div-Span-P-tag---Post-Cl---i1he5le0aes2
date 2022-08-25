c72b3d181d5a50a78d5842445154da31e9279abb
Ratneshdubey07 <111305768+Ratneshdubey07@users.noreply.github.com>
2022-08-25 20:21:19 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
-/const content = document.querySelectorAll('phone')
-const contents = document.querySelectorAll('.content');
-const listItems = document.querySelectorAll('nav ul li');
-console.log(contents, listItems)
-listItems.forEach((item, idx) => {
-    item.addEventListener('click', () => {
+const contents = document.querySelectorAll('.content')
+const listItems = document.querySelectorAll('nav ul li')
+
+listItems.forEach((item,idx)=>{
+    item.addEventListener('click',()=>{
         hideAllContents()
         hideAllItems()
-
         item.classList.add('active')
         contents[idx].classList.add('show')
     })
 })
-
 function hideAllContents() {
-    contents.forEach(content => content.classList.remove('show'))
+    contents.forEach(content=>content.classList.remove('show'))
+}
+function hideAllItems() {
+    listItems.forEach(item=>item.classList.remove('active'))
 }
 
-function hideAllItems() {
-    listItems.forEach(item => item.classList.remove('active'))
-}
